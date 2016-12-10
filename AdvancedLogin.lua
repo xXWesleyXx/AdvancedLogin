79a7dbf081b54447d0ce435641562b39ee1a07fc
xXWesleyXx <tylerseymour14@gmail.com>
2016-12-09 21:33:23 -0800

    Update AdvancedLogin.lua

--- AdvancedLogin.lua
+++ AdvancedLogin.lua
@@ -7,11 +7,33 @@ os.loadAPI('AL/ALAPI')
 cfgPath = 'AL/Config'
 cfgHandle = fs.open(cfgPath, 'r')
 cfg = textutils.unserialise()
-
+success = false
 --[[ Functions ]]--
 
 function lock()
   uiPath = 'AL/UI'
   uiHandle = fs.open(uiPath, 'r')
   print(uiHandle.readAll())
+  term.setCursorPos(13, 6)
+  UN = read()
+  if UN ~= cfg.UN then
+    term.setCursorPos(7, 14)
+    print(cfg.Log.error.UN.invalid)
+    sleep(3)
+    os.reboot()
+  else
+    term.setCursorPos(13, 10)
+    PW = read()
+    if PW ~= cfg.PW then
+      term.setCursorPos(7, 14)
+      print(cfg.Log.error.PW.invalid)
+      sleep(3)
+      os.reboot()
+    else
+      success = true
 end
+
+while success do 
+      success = false
+      term.clear()
+end      
